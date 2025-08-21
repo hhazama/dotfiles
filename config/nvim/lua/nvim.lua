41cab235a674aab975d121bcf2e9adf0917e1ac2
hazama <hiroaki_hazama@forcia.com>
2025-08-21 17:11:06 +0900

    update

--- config/nvim/lua/nvim.lua
+++ config/nvim/lua/nvim.lua
@@ -935,11 +935,11 @@ require('lazy').setup {
         },
         -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
+          bottom_search = false, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be nt to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          lsp_doc_border = true, -- add a border to hover docs and signature help
         },
       }
     end,
@@ -952,7 +952,7 @@ require('lazy').setup {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
+      --'rcarriga/nvim-notify',
     },
   },
 
